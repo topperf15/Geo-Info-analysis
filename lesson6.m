35276a8baa03447320f9e7e018a382023695b240
Topper <142811385+topperf15@users.noreply.github.com>
2023-10-16 10:26:03 -0400

    Update lesson6.m

--- lesson6.m
+++ lesson6.m
@@ -2,10 +2,10 @@
 % based on lesson 2
 % read in the shapefiles
 % WGS84 UTM 18N is EPSG 32618
-txCo = readgeotable(txco.shp); //alt is shapefileread
+txCo = readgeotable(txco.shp); %alt is shapefileread
 txSeat = readgeotable(txseat.shp);
 figure
-axesm('eqaconic','MapLatLimit',[38,40],'MapLonLimit',[-79,-76]) // what is is for TX
+axesm('eqaconic','MapLatLimit',[38,40],'MapLonLimit',[-79,-76]) % what is is for TX
 %geoshow(lat,lon) projects and displays the latitude and longitude vectors lat and lon 
 %using the projection stored in the current axesm-based map (previously referred to as map axes).
 %If there is no current axesm-based map, then lat and lon are projected
@@ -14,11 +14,11 @@ axesm('eqaconic','MapLatLimit',[38,40],'MapLonLimit',[-79,-76]) // what is is fo
 geoshow(txCo)
 geoshow(txSeat)
 %buffer the seats
-[latb,lonb] = bufferm(txSeat.lat,txSeat.lon,km2deg(200))
+[latb,lonb] = bufferm(txSeat.lat,txSeat.lon,km2deg(200));
 geoshow(latb.lonb,'k--')
 %%drop a circle on an input point
 [latin,lonin] = inputm(1);
-circrad = km2deg(10); //10 km in deg
+circrad = km2deg(10); %10 km in deg
 [lats,lons] = scircle1(latin,lonin,circrad);
 geoshow(lats,lons)
 %%new figure with georaster play with sea surface temps
