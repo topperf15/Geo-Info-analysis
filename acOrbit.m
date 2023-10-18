1d75e8997f166f576d1f2b6c1d17c8115c3cb198
Topper <142811385+topperf15@users.noreply.github.com>
2023-10-17 21:30:43 -0400

    Update acOrbit.m

--- acOrbit.m
+++ acOrbit.m
@@ -2,6 +2,6 @@ vel = 200; %nm per hr (kts)
 theta = 15; %degrees bank angle
 acTurnRad = vel^2/(11.26*tan(theta); %feet
 [latin,lonin] = inputm(2);
-circrad = nm2deg(acTurnRad/6000); 
+circrad = nm2deg(acTurnRad/6076); 
 [latb,lonb] = bufferm(latin,lonin,circrad);
 geoshow(lats,lons)
