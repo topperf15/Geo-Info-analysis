7cb30da4d8f9fc7f3033026eb84ed0b0293363c4
Topper <142811385+topperf15@users.noreply.github.com>
2023-10-17 21:31:39 -0400

    Update acOrbit.m

--- acOrbit.m
+++ acOrbit.m
@@ -4,4 +4,4 @@ acTurnRad = vel^2/(11.26*tan(theta); %feet
 [latin,lonin] = inputm(2);
 circrad = nm2deg(acTurnRad/6076); 
 [latb,lonb] = bufferm(latin,lonin,circrad);
-geoshow(lats,lons)
+geoshow(latb,lonb)
