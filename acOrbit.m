bd5f5a6b019d3c77de19ff563fc7ee5d67545b55
Topper <142811385+topperf15@users.noreply.github.com>
2023-10-20 18:01:29 -0400

    Update acOrbit.m

--- acOrbit.m
+++ acOrbit.m
@@ -1,6 +1,6 @@
 vel = 200; %nm per hr (kts)
 theta = 15; %degrees bank angle
-acTurnRad = vel^2/(11.26*tan(theta); %feet
+acTurnRad = vel^2/(11.26*tand(theta); %feet
 [latin,lonin] = inputm(2);
 circrad = nm2deg(acTurnRad/6076); 
 [latb,lonb] = bufferm(latin,lonin,circrad);
