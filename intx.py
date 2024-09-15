3eed42e124ab7b694fe5e86f5a564a74e4891cac
Topper <142811385+topperf15@users.noreply.github.com>
2024-09-15 18:13:06 -0400

    Update intx.py

--- intx.py
+++ intx.py
@@ -8,8 +8,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def move(in_pos,in_spd,in_hdg):
-    [x,y] = np.add(in_pos,in_spd*np.array([np.cos(np.radians(in_hdg)), np.sin(np.radians(in_hdg))]))
-    return [x,y]
+    [x,y,h] = np.add(in_pos,in_spd*np.array([np.cos(np.radians(in_hdg)), np.sin(np.radians(in_hdg)),0]))
+    return [x,y,h]
     
 intx = 'cata'
 arc = np.arange(-60,60,1)
@@ -29,20 +29,23 @@ while bf[0] < rf[0]:
     pos = np.subtract(rf, bf)
     rng = np.linalg.norm(pos)
     brg = np.degrees(np.arctan(pos[1]/pos[0]))
+    el = np.degrees(np.arctan(pos[2]/pos[0]))
     ata = bfh - brg
     aa = rfh - brg
     ax3.plot(bf[0],aa,'bo')
     ax4.plot(bf[0],rng,'b^')
     cc = 180 - rfh + brg
-    ax1.plot(ata, rng, 'gs')
+    ax1.plot(ata, el, 'gs')
     # ax2.plot(rf[0],rf[1],'r<')
     # ax2.plot(bf[0],bf[1],'b>')
     ax2.quiver(rf[0],rf[1],rfs*np.cos(np.radians(rfh)),rfs*np.sin(np.radians(rfh)),color='r',pivot='tip')
     ax2.quiver(bf[0],bf[1],bfs*np.cos(np.radians(bfh)),bfs*np.sin(np.radians(bfh)),color='b',pivot='tip')
     if intx == 'pure':
         bfh += -ata
-    elif intx == 'cata':
+    elif intx == 'hot':
         bfh = brg + 1.5*cc
+    else:
+        bfh = brg + cc
     bf = move(bf,bfs,bfh)
     rf = move(rf,rfs,rfh)
 
